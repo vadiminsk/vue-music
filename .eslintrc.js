13764cdf68e7af69744eb2ebed43b0d9e9839212
Vadim <vadiminsk90@gmail.com>
2021-09-10 16:33:53 +0300

    configurate styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,27 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+
+  extends: ["plugin:vue/vue3-essential", "@vue/airbnb"],
+
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": "off",
+    "no-debugger": "off",
+    quotes: [2, "single", { avoidEscape: true, allowTemplateLiterals: true }],
   },
+
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
         jest: true,
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+/* eslint-disable quotes */
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/tailwind.css";
+import "./assets/main.css";
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount("#app");
